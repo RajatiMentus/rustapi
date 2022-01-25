e0b37bd0d7e6a0ab793a054617b924bb14b0c998
RajatiMentus <98326858+RajatiMentus@users.noreply.github.com>
2022-01-25 22:26:17 +0530

    Update dbHelper.rs
    change1

--- src/dbHelper.rs
+++ src/dbHelper.rs
@@ -14,12 +14,12 @@ pub struct question {
 
 pub fn Get() -> Result<Vec<question>, Error> {
     println!("Connecting DB");
-    let mut client = Client::connect("postgresql://postgres:rajat123@localhost/Questions_db", NoTls)?;
+    let mut client = Client::connect("postgres://thnqlzvzhzeojv:4710ac46c7623591974c500bc3248be30d317cfea6cb0c470cdf766c297820ac@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d1u926tc2bcjcv", NoTls)?;
 
     let id: u32 = 1;
     println!("Connection Successful");
     let mut arr:Vec<question> = Vec::new();
-    for row in client.query("SELECT \"ID\", \"Text\" FROM public.\"Question\";", &[])? {           
+    for row in client.query("SELECT \"ID\", \"Text\" FROM public.\"Questions\";", &[])? {           
         arr.push(question{
             ID: row.get(0),
             Text: row.get(1),
